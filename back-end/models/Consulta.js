620f4c73ec2947f46664016812f3cd7374006ebd
rcclaro23 <renatocclaro@gmail.com>
2020-09-28 15:24:20 +0000

    (28/09) Inicio do Projeto

---
+++ back-end/models/Consulta.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const esquema = mongoose.Schema({
+    data: { type: Date, required: true },
+    hora: { type: String, required: true }, 
+    valor: { type: Number, required: true }
+    
+    }),
+    
+     
+
+// PARÂMETROS DO mongoose.model()
+// 1º -> Nome do model (inicial maiúscula, igual ao nome do arquivo)
+// 2º -> a constante esquema, montada anteriormente
+// 3º -> o nome da COLEÇÃO no BD que irá receber os objetos que serão
+//       criados a partir deste model (inicial minúscula, plural do
+//       nome do model)
+
+;
+module.exports = mongoose.model('Consulta', esquema, 'consultas')
