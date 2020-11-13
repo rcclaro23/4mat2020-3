66472f67418ce06b843b6a45c2e552e7234802d1
rcclaro23 <renatocclaro@gmail.com>
2020-11-13 21:50:23 +0000

    (11/11) Término do Com de Listagem e Inicio do Comp de Formulário

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CursoListComponent } from './curso/curso-list/curso-list.component';
+import { CursoFormComponent } from './curso/curso-form/curso-form.component';
 
 //import { CursoListComponent} from './curso/curso-list/curso-list.component';
 
 const routes: Routes = [
     // rotas no angular Nunca começam com /
-   { path: 'curso', component: CursoListComponent}
+   { path: 'curso', component: CursoListComponent},
+   {path: 'curso/novo', component: CursoFormComponent},
+   {path: 'curso/:id', component: CursoFormComponent}
 ];
 
 @NgModule({
--- front-end/src/app/curso/curso-list/curso-list.component.ts
+++ front-end/src/app/curso/curso-list/curso-list.component.ts
@@ -1,5 +1,7 @@
+
 import { CursoService } from '../curso.service';
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-curso-list',
@@ -7,13 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./curso-list.component.scss']
 })
 export class CursoListComponent implements OnInit {
+
     cursos: any = [] // vetor vazio
-    displayedColumns : string[] = ['nome', 'carga_horaria', 'nivel', 'valor_curso' ]
-  constructor(private cursoSrv : CursoService) { }
+
+    displayedColumns : string[] = ['nome', 'carga_horaria', 'nivel', 'valor_curso',
+    'editar','excluir']
+    
+  constructor(
+      private cursoSrv : CursoService,
+      private snackBar : MatSnackBar
+      ) { }
 
   async ngOnInit() {
       this.cursos = await this.cursoSrv.listar()
       console.log(this.cursos)
   }
+    async excluir(id: string){
+    if(confirm('Deseja realmente excluir este ítem?')){
+    try{
+     // 1) Efetuar a exclusão 
+     await this.cursoSrv.excluir(id)
+     // 2) Atualizar os dados da tabela
+     this.ngOnInit()
+     // 3)Dar um feed back de sucesso para o usuario 
+     this.snackBar.open('Item excluído com sucesso.', 'Entendi',{
+         duration: 5000 // 5 segundos
+     })
+    }
+    catch(erro){
+        console.error(erro)
+    //  4) Dar um feedback de erro para o usuario
+    this.snackBar.open('Erro: não foi possível excluir este ítem.', 'Que pena', {
+        duration: 5000 // 5 segundos   
+    })
 
+
+
+    }
+}
+}
 }
