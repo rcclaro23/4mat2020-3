56a1b908eef13f474d0b260339c1ae52967b26b0
rcclaro23 <renatocclaro@gmail.com>
2020-11-20 20:40:24 +0000

    (18/11) Término dos primeiros componentes + formulário

--- front-end/src/app/curso/curso-form/curso-form.component.ts
+++ front-end/src/app/curso/curso-form/curso-form.component.ts
@@ -1,4 +1,9 @@
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CursoService } from './../curso.service';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-curso-form',
@@ -7,20 +12,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursoFormComponent implements OnInit {
 
-title: string = 'Novo Curso'
+  title : string = 'Novo curso'
 
-curso: any = {} //Objeto vazio, nome da entidade no Singular
-niveis: any = [
-{valor: 'Básico', descr: 'Básico'},
-{valor: 'Intermediário', descr: 'Intermediário'},
-{valor: 'Avançado', descr: 'Avançado'}
-]
-  constructor() { }
+  curso : any = {} // Objeto vazio, nome da entidade no SINGULAR
 
-  ngOnInit(): void {
+  niveis : any = [
+    { valor: 'Básico', descr: 'Básico' },
+    { valor: 'Intermediário', descr: 'Intermediário' },
+    { valor: 'Avançado', descr: 'Avançado' }
+  ]
+
+  constructor(
+    private cursoSrv : CursoService,
+    private snackBar : MatSnackBar,
+    private location : Location,
+    private actRoute : ActivatedRoute
+  ) { }
+
+  async ngOnInit() {
+    // Verificando se existe id na rota que trouxe ao formulário
+    if(this.actRoute.snapshot.params['id']) {
+      try {
+        // 1) Trazer o registro do back-end para edição
+        this.curso = await this.cursoSrv.obterUm(this.actRoute.snapshot.params['id'])
+        // 2) Mudar o título da página
+        this.title = 'Editando curso'
+      }
+      catch(erro) {
+        console.log(erro)
+        this.snackBar.open('ERRO: não foi possível carregar os dados para edição.',
+          'Que pena!', { duration: 5000 })
+      }
+    }
   }
-salvar(form){
 
-}
+  async salvar(form : NgForm) {
+    try {
+      if(form.valid) {
+        // 1) Enviar os dados para o back-end para serem salvos
+        if(this.curso._id) {
+          // _id existe, esse registro já foi salvo anteriormente
+          // no BD e é caso de atualização
+          await this.cursoSrv.atualizar(this.curso)
+        }
+        else {
+          await this.cursoSrv.novo(this.curso)
+        }
+        // 2) Dar um feedback (mensagem) para o usuário
+        this.snackBar.open('Dados salvos com sucesso.', 'Entendi',
+          { duration: 5000 })
+        // 3) Voltar para a tela de listagem
+        this.location.back()
+      }
+    }
+    catch(erro) {
+      console.log(erro)
+      this.snackBar.open('ERRO: não foi possível salvar os dados.', 'Que pena!',
+        { duration: 5000 })
+    }
+  }
+
+  voltar(form : NgForm) {
+    let result = true
+    // form.dirty = formulário "sujo", não salvo (via código)
+    // form.touched = o conteúdo de algum campo foi alterado (via usuário)
+    if(form.dirty && form.touched) {
+      result = confirm('Há dados não salvos. Deseja realmente voltar?')
+    }
+    // Retorna à página anterior se resposta foi positiva ou se o formulário
+    // estiver "limpo"
+    if(result) this.location.back()
+  }
 
 }
--- front-end/src/app/curso/curso.service.ts
+++ front-end/src/app/curso/curso.service.ts
@@ -15,8 +15,21 @@ export class CursoService {
   listar() {
     return this.http.get(this.apiUri).toPromise()
   }
-excluir(id: string){
-    return this.http.request('DELETE', this.apiUri, {body:{_id: id}}).toPromise()
-}
+
+  excluir(id : string) {
+    return this.http.request('DELETE', this.apiUri, {body: {_id: id}}).toPromise()    
+  }
+
+  novo(body : any) {
+    return this.http.post(this.apiUri, body).toPromise()
+  }
+
+  obterUm(id: string) {
+    return this.http.get(this.apiUri + '/' + id).toPromise()
+  }
+
+  atualizar(body : any) {
+    return this.http.put(this.apiUri, body).toPromise()
+  }
 
 }
